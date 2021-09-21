29a64e2d7f9127cfb68f94f829b4896ead1f222b
Inseo Hwang <sjsjsj1246@gmail.com>
2021-09-21 17:17:43 +0900

    Feat: Login 화면 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import { registerRootComponent } from "expo";
 import { StatusBar } from "expo-status-bar";
 import { extendTheme, NativeBaseProvider } from "native-base";
-import React from "react";
-import Login from "./components/Login";
+import React, { useEffect, useState } from "react";
+import { Intro, Login } from "./components/Login";
 import * as Font from "expo-font";
 import customTheme from "./hooks/customTheme";
-
-Font.loadAsync({
-  "apple-sd-gothic-neo-medium": require("./assets/fonts/AppleSDGothicNeoM.ttf"),
-  "apple-sd-gothic-neo-light": require("./assets/fonts/AppleSDGothicNeoL.ttf"),
-  "apple-sd-gothic-neo-bold": require("./assets/fonts/AppleSDGothicNeoB.ttf"),
-  "sf-compact-display": require("./assets/fonts/SfCompactDisplay.otf"),
-  "sf-compact-display-light": require("./assets/fonts/SfCompactDisplay-light.otf"),
-  "sf-compact-display-bold": require("./assets/fonts/SfCompactDisplay-bold.ttf"),
-});
+import Onboarding from "./components/Onboarding";
 
 const App = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    // 홈 화면을 렌더링할 데이터를 로딩하거나. 인증 절차등을 처리합니다. 성공시에 로딩화면에서 벗어납니다.
+    const loadFont = async () => {
+      await Font.loadAsync({
+        "apple-sd-gothic-neo-medium": require("./assets/fonts/AppleSDGothicNeoM.ttf"),
+        "apple-sd-gothic-neo-light": require("./assets/fonts/AppleSDGothicNeoL.ttf"),
+        "apple-sd-gothic-neo-bold": require("./assets/fonts/AppleSDGothicNeoB.ttf"),
+        "sf-compact-display": require("./assets/fonts/SfCompactDisplay.otf"),
+        "sf-compact-display-light": require("./assets/fonts/SfCompactDisplay-light.otf"),
+        "sf-compact-display-bold": require("./assets/fonts/SfCompactDisplay-bold.ttf"),
+      });
+      setLoading(false);
+    };
+    loadFont();
+  }, []);
+
   return (
     <NativeBaseProvider theme={customTheme}>
-      <Login />
+      {loading ? null : <Onboarding />}
       <StatusBar />
     </NativeBaseProvider>
   );
