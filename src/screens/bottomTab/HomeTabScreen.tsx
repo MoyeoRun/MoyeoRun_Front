142dc25b5436bc587f22c19565d57f9ace056ef3
Inseo Hwang <sjsjsj1246@gmail.com>
2021-10-07 04:54:31 +0900

    Refactor: Screen제거

--- src/screens/bottomTab/HomeTabScreen.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import HomeContainer from "../../containers/HomeTabContainer";
-
-const HomeTabScreen = () => {
-  return <HomeContainer />;
-};
-
-export default HomeTabScreen;
--- src/screens/bottomTab/RunningTabScreen.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import RunningTabContainer from '../../containers/RunningTabContainer';
-
-const RunningTabScreen = () => {
-  return <RunningTabContainer />;
-};
-
-export default RunningTabScreen;
