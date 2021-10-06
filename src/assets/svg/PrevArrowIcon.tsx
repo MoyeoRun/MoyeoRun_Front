70ae492927dbd875f1fdfe931f2ed1a4eead6a69
Inseo Hwang <sjsjsj1246@gmail.com>
2021-10-07 04:55:17 +0900

    Refactor: svg index 추가

--- src/assets/svg/PrevArrowIcon.tsx
+++ src/assets/svg/PrevArrowIcon.tsx
@@ -1,17 +1,16 @@
-import { Factory } from "native-base";
-import React from "react";
-import { SvgXml } from "react-native-svg";
+import { Factory } from 'native-base';
+import React from 'react';
+import { SvgXml } from 'react-native-svg';
 
-
-const RecordIcon = ({ color, ...props }: { color: string }) => {
-    const source = `
+const PrevArrowIcon = ({ color, ...props }: { color: string }) => {
+  const source = `
     <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M25.2084 15.5H5.20837" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     <path d="M12.5 22.7917L5.20837 15.5L12.5 8.20837" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
     `;
-    const FactoryView = Factory(SvgXml);
-    return <FactoryView xml={source} {...props} />;
-  };
-  
-  export default RecordIcon;
+  const FactoryView = Factory(SvgXml);
+  return <FactoryView xml={source} {...props} />;
+};
+
+export default PrevArrowIcon;
---
+++ src/assets/svg/index.ts
@@ -0,0 +1,17 @@
+export { default as AppleIcon } from './AppleIcon';
+export { default as FriendIcon } from './FriendIcon';
+export { default as GoogleIcon } from './GoogleIcon';
+export { default as HomeIcon } from './HomeIcon';
+export { default as KakaoIcon } from './KakaoIcon';
+export { default as LogoBlue } from './LogoBlue';
+export { default as LogoMini } from './LogoMini';
+export { default as LogoWhite } from './LogoWhite';
+export { default as MissionIcon } from './MissionIcon';
+export { default as NaverIcon } from './NaverIcon';
+export { default as ParticipantIcon } from './ParticipantIcon';
+export { default as PlusIcon } from './PlusIcon';
+export { default as PrevArrowIcon } from './PrevArrowIcon';
+export { default as RecordIcon } from './RecordIcon';
+export { default as RightArrow } from './RightArrow';
+export { default as RunningIcon } from './RunningIcon';
+export { default as SmallRunningIcon } from './SmallRunningIcon';
