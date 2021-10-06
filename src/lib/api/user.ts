3d99cc6726e73d1f8819656ca79fd0ad918e353d
Inseo Hwang <sjsjsj1246@gmail.com>
2021-10-07 04:19:25 +0900

    Fix: API return값 수정

--- src/lib/api/user.ts
+++ src/lib/api/user.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const updateBodyInfo = async ({ name, height, weight }: { name?: string; height?: number; weight?: number }) => {
   const response = await axios.patch('http://45.248.73.50:30007/user', { name, height, weight });
-  return response;
+  return response.data;
 };
