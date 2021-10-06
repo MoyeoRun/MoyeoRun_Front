542b96ab418ed69c9a6b73f27e53e3bafa9f6645
Inseo Hwang <sjsjsj1246@gmail.com>
2021-10-07 04:20:19 +0900

    Feature: 리덕스 설계

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,29 +1,38 @@
 import { createAction, handleActions } from 'redux-actions';
 import { pender } from 'redux-pender';
+import * as authAPI from '../lib/api/auth';
 
 const KAKAO_OAUTH = 'auth/KAKAO_OAUTH' as const;
 const GOOGLE_OAUTH = 'auth/GOOGLE_OAUTH' as const;
 const NAVER_OAUTH = 'auth/NAVER_OAUTH' as const;
 
-export const kakaoOauth = createAction(KAKAO_OAUTH);
-export const googleOauth = createAction(GOOGLE_OAUTH);
-export const naverOauth = createAction(NAVER_OAUTH);
+export const kakaoOauth = createAction(KAKAO_OAUTH, authAPI.kakaoOauth);
+export const googleOauth = createAction(GOOGLE_OAUTH, authAPI.googleOauth);
+export const naverOauth = createAction(NAVER_OAUTH, authAPI.naverOauth);
 
 type AuthAction = ReturnType<typeof kakaoOauth> | ReturnType<typeof googleOauth> | ReturnType<typeof naverOauth>;
 
 type AuthState = {
   auth: null | object;
+  accessToken: null | string;
+  refreshToken: null | string;
 };
 
 const initialState: AuthState = {
   auth: null,
+  accessToken: null,
+  refreshToken: null,
 };
 
 export default handleActions(
   {
     ...pender({
       type: KAKAO_OAUTH,
-      onSuccess: (state, { payload }) => {},
+      onSuccess: (state: AuthState, { payload }) => ({
+        ...state,
+        accessToken: payload.token.accessToken,
+        refreshToken: payload.token.refreshToken,
+      }),
     }),
   },
   initialState,
